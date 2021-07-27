2c757811499d5f910bf47b455753583ed99bc445
MoaazElneshawy <m.elneshawy@gmail.com>
2021-07-27 12:08:49 +0300

    use provider to handle state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopapp/pages/product_review_page.dart';
+import 'package:shopapp/providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +11,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
-        fontFamily: 'Lato',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          fontFamily: 'Lato',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductsReviewPage(),
       ),
-      home: ProductsReviewPage(),
     );
   }
 }
