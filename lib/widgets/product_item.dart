192132750a9580e3c2884cdc0a652c2842f89fb7
MoaazElneshawy <m.elneshawy@gmail.com>
2021-07-27 10:43:27 +0300

    force child into radius corners using ClipRRect widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopapp/models/product.dart';
+
+import '../models/product.dart';
 
 class ProductItem extends StatelessWidget {
   final Product _product;
@@ -8,26 +9,29 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        _product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: _product.isFavroite
-              ? Icon(Icons.favorite)
-              : Icon(Icons.favorite_border),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          _product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          _product.title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: _product.isFavroite
+                ? Icon(Icons.favorite)
+                : Icon(Icons.favorite_border),
+            onPressed: () {},
+          ),
+          title: Text(
+            _product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
         ),
       ),
     );
