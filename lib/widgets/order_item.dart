1bc55ac29412e92cc5f75ce2ffee9ce9182418b5
MoaazElneshawy <m.elneshawy@gmail.com>
2021-08-04 09:31:40 +0300

    display order products

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -2,20 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shopapp/providers/orders.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem orderItem;
 
   OrderItemWidget(this.orderItem);
 
   @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var _isExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text("\$${orderItem.amount}"),
-      subtitle: Text(DateFormat("dd MM yyyy hh:mm").format(orderItem.dateTime)),
-      trailing: IconButton(
-        icon: Icon(Icons.expand_more),
-        onPressed: () {},
-      ),
-    );
+    return Card(
+        margin: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            ListTile(
+                title: Text("\$${widget.orderItem.amount}"),
+                subtitle: Text(DateFormat("dd MM yyyy hh:mm")
+                    .format(widget.orderItem.dateTime)),
+                trailing: IconButton(
+                  icon:
+                      Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+                  onPressed: () {
+                    setState(() {
+                      _isExpanded = !_isExpanded;
+                    });
+                  },
+                )),
+            Divider(height: 2),
+            if (_isExpanded)
+              ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (_, index) => ListTile(
+                  title: Text(widget.orderItem.products[index].title),
+                  subtitle: Text("\$${widget.orderItem.products[index].price}"),
+                  trailing:
+                      Text("x${widget.orderItem.products[index].quantity}"),
+                ),
+                itemCount: widget.orderItem.products.length,
+              )
+          ],
+        ));
   }
 }
